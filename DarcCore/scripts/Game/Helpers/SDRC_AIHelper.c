94d87184b353178e96cd4e1737be112a4bfcc399
mokdevel <mokdevel@hotmail.com>
2025-08-15 20:37:46 +0300

    Faction things

--- DarcCore/scripts/Game/Helpers/SDRC_AIHelper.c
+++ DarcCore/scripts/Game/Helpers/SDRC_AIHelper.c
@@ -453,7 +453,55 @@ sealed class SDRC_AIHelper
 		
 		return factionKey;
 	}		
+
+	//------------------------------------------------------------------------------------------------
+	/*!
+	Get factionlist
+	*/	
+	static int GetFactionList(out array<string> factionList, bool printList = false)
+	{
+		array<Faction> factions = {};
+		SCR_FactionManager factionManager = SCR_FactionManager.Cast(GetGame().GetFactionManager());
+		
+		if (!factionManager)
+		{			
+			return 0;
+		}
+		
+		factionManager.GetFactionsList(factions);
+
+		foreach (Faction faction : factions)
+		{
+//			string factionName = SCR_StringHelper.Translate(faction.GetFactionName());
+			string factionName = faction.GetFactionKey();
+			factionList.Insert(factionName);
 			
+			if (printList)
+			{
+				SDRC_Log.Add("[SDRC_Misc:GetFactionList] Faction found: " + factionName, LogLevel.NORMAL);				
+			}
+		}
+				
+		return factionList.Count();
+	}
+
+	//------------------------------------------------------------------------------------------------
+	/*!
+	Get Faction with string name
+	*/	
+	static Faction GetFactionWithName(string name)
+	{
+		FactionManager factionManager = GetGame().GetFactionManager();
+		if (!factionManager)
+			return null;
+		
+		Faction faction = factionManager.GetFactionByKey(name);
+		if (!faction)
+			return null;
+		
+		return faction;
+	}						
+				
 	//------------------------------------------------------------------------------------------------
 	/*!
 	Delete an AI
