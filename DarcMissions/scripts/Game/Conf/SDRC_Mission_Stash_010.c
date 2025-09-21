292261a48514d245f536fd0552713feabc8a35fe
mokdevel <mokdevel@hotmail.com>
2025-09-21 21:00:31 +0300

    New loadable stash mission

---
+++ DarcMissions/scripts/Game/Conf/SDRC_Mission_Stash_010.c
@@ -0,0 +1,83 @@
+//Mission SDRC_Mission_Stash_010.c
+
+//------------------------------------------------------------------------------------------------
+/*!
+This mission spawns a stash for loot. This essentially the same as Occupation mission.if
+
+The usage for Stash mission is mainly as a story ending when using DarcStories.
+*/
+
+//------------------------------------------------------------------------------------------------
+class SDRC_Stash_010_JsonApi : SDRC_StashJsonApi
+{
+	const string DC_MISSIONCONFIG_FILE = "dc_missionConfig_Stash_010.json";
+
+	void SDRC_Stash_010_JsonApi(string fileName = "")
+	{		
+		SetFileName(DC_MISSIONCONFIG_FILE);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override void SetDefaults()
+	{
+		//Default
+		conf.missionCycleTime = SDRC_MISSION_CYCLE_TIME_DEFAULT;
+		conf.activeDistance = 50;
+		conf.missionList = {0};
+		//Mission specific		
+		//----------------------------------------------------
+		conf.Stashs.Insert(Stash10());				
+	};
+	
+	//----------------------------------------------------
+	SDRC_Camp Stash10()
+	{
+		ref SDRC_Camp stash = new SDRC_Camp();
+		stash.general.Set(
+			10, "index 10: Random stash",
+			{"0 0 0"},
+			"any",
+			"A stash near %l",
+			"Loot is yours to take",
+			DC_EMissionWinCondition.FIND_IN_15,
+			"Loot found.",
+			"Loot lost.",
+			"",
+			"DARC_MISSION", DC_EMissionIcon.GM_MISSION_STASH_MAP,
+			0
+		);
+		
+		stash.Set(
+			{
+			},
+			{0, 0},
+			{50, 300},
+			DC_EWaypointGenerationType.RANDOM,
+			DC_EWaypointMoveType.PATROLCYCLE,
+			{
+			},
+			50, 1.0,
+			3
+		);
+		
+		ref SDRC_Loot loot = new SDRC_Loot();
+		array<string> lootItems = {
+				"WEAPON_RIFLE",
+				"WEAPON_HANDGUN", "WEAPON_HANDGUN", "WEAPON_HANDGUN",
+				"UTIL_ATTACHMENT",
+				"ITEM_MEDICAL",
+				"ITEM_GENERAL", "ITEM_GENERAL", "ITEM_GENERAL", "ITEM_GENERAL", "ITEM_GENERAL", "ITEM_GENERAL",
+			};
+		loot.Set(0.7, lootItems);
+		stash.loot = loot;
+		
+		ref SDRC_Structure item_0 = new SDRC_Structure();
+		item_0.Set(
+			"{4A9E0C3D18D5A1B7}Prefabs/Props/Crates/LootCrateWooden_01.et",
+			"100.47 1 144.562"
+		);
+		stash.campItems.Insert(item_0);
+		
+		return stash;
+	};
+}
