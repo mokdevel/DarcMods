8a88ec53fa43c059fa65090506acd97e41c11de7
mokdevel <mokdevel@hotmail.com>
2025-10-15 21:52:53 +0300

    Compat class

---
+++ DarcMissions/scripts/Game/Helpers/SDRC_Compat.c
@@ -0,0 +1,18 @@
+//Helpers SDRC_Compat
+
+//------------------------------------------------------------------------------------------------
+/*!
+Various things for compat support. You should override this.
+*/
+
+class SDRC_Compat
+{
+	//------------------------------------------------------------------------------------------------
+	/*!
+	Add you init code here.
+	*/
+	static bool Init()
+	{
+		return true;
+	}
+}
