ab986a99d04511facdda3a4b62dadcaa19657619
mokdevel <mokdevel@hotmail.com>
2025-12-25 12:58:13 +0200

    Restructuring

---
+++ DarcMissions/scripts/Game/Mission/SDRC_MissionConfigAi.c
@@ -0,0 +1,98 @@
+//------------------------------------------------------------------------------------------------
+// AI CONFIG
+//------------------------------------------------------------------------------------------------
+class SDRC_MissionConfigAi : Managed
+{
+	private ref array<int> count = {};
+	ref array<string> types = {}; 			//The prefab names of AI groups or characters. The AI is randomly picked from this list.
+	private int skill;						//Skill for AI (0-100). See SCR_AICombatComponent and EAISkill
+	private float perception;
+	ref array<int> waypointRange = {};		//min, max
+	SDRC_EWaypointGenerationType waypointGenType;
+	SDRC_EWaypointMoveType waypointMoveType;
+	
+	void Set(array<int> count_, array<string> types_, int skill_, float perception_, array<int> waypointRange_, SDRC_EWaypointGenerationType waypointGenType_, SDRC_EWaypointMoveType waypointMoveType_)
+	{
+		count = count_;
+		types = types_;
+		skill = skill_;
+		perception = perception_;		
+		waypointRange = waypointRange_;
+		waypointGenType = waypointGenType_;
+		waypointMoveType = waypointMoveType_;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	/*! Return the count of AIs for the mission 
+	
+	NOTE: mission and missionFrame difficulty affects the outcome	
+	*/	
+	int GetCount(SDRC_EMissionDifficulty difficulty = SDRC_EMissionDifficulty.NORMAL)
+	{
+		int cnt = SDRC_Misc.RandomInt(count[0], count[1]);
+		
+		SCR_BaseGameMode m_BaseGameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());			
+		if (m_BaseGameMode)
+		{
+			float coef = m_BaseGameMode.missionFrame.m_Config.missionDifficulty.aiCountCoef[difficulty];
+			
+			int low = count[0] * coef;
+			if (low < count[0])
+			{
+				low = count[0];
+			}
+			int high = count[1] * coef;
+			if (high < count[1])
+			{
+				high = count[1];
+			}
+			cnt = SDRC_Misc.RandomInt(low, high);
+		}
+		
+		return cnt;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	/*! Return a proper skill value that matches EAISkill. 
+	
+	NOTE: mission and missionFrame difficulty affects the outcome	
+	*/	
+	int GetSkill(SDRC_EMissionDifficulty difficulty = SDRC_EMissionDifficulty.NORMAL)
+	{
+		int sk = skill;
+		
+		SCR_BaseGameMode m_BaseGameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());			
+		if (m_BaseGameMode)
+		{
+			float coef = m_BaseGameMode.missionFrame.m_Config.missionDifficulty.aiSkillCoef[difficulty];
+			sk = skill * coef;
+		}		
+		
+		if (sk < 10) 				{sk = EAISkill.NOOB;}			
+		if (sk >= 20 && sk < 50)	{sk = EAISkill.ROOKIE;}
+		if (sk >= 50 && sk < 70)	{sk = EAISkill.REGULAR;}
+		if (sk >= 70 && sk < 80)	{sk = EAISkill.VETERAN;}
+		if (sk >= 80 && sk < 100)	{sk = EAISkill.EXPERT;}
+		if (sk > 100)				{sk = EAISkill.CYLON;}
+		
+		return sk;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	/*! Return a proper perception value. 
+	
+	NOTE: mission and missionFrame difficulty affects the outcome	
+	TBD: I'm a little unsure how this in total affects, but I presume the higher is better.
+	*/
+	float GetPerception(SDRC_EMissionDifficulty difficulty = SDRC_EMissionDifficulty.NORMAL)
+	{
+		float perc = perception;
+		SCR_BaseGameMode m_BaseGameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());			
+		if (m_BaseGameMode)
+		{
+			float coef = m_BaseGameMode.missionFrame.m_Config.missionDifficulty.aiPerceptionCoef[difficulty];
+			perc = perception * coef;
+		}		
+		return perception;
+	}			
+}
---
+++ DarcMissions/scripts/Game/Mission/SDRC_MissionConfigGeneral.c
@@ -0,0 +1,70 @@
+//------------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------------
+// GENERAL CONFIG
+//------------------------------------------------------------------------------------------------
+class SDRC_MissionConfigGeneral : Managed
+{
+	int subIdx;								//Unique index for the sub mission. 
+	string comment;							//Generic comment to describe the mission. Not used in game.
+	ref array<vector> pos = {};				//Positions for mission. "0 0 0" used for random location chosen from locationTypes. First is mission position, second is destination for missions that need it.
+	float size;								//The size (radius) of the empty space needed to decide on a mission position.
+	ref array<EMapDescriptorType> locationTypes = {};
+	string posName;							//Your name for the mission location (like "Harbor near city"). "any" uses location name found from locationTypes 
+	string title;							//Title for the hint shown for players
+	string info;							//Details for the hint shown for players
+	SDRC_EMissionWinCondition winCondition;	//Mission win condidition
+	string winMessage;						//Message to show when mission is completed
+	string loseMessage;						//Message to show when mission fails
+	string faction;							//Faction for the mission. Setting as empty, works as the default to select from the enemyFactions
+	string markerType;						//Marker type for the mission
+	int markerIcon;							//Marker ID within markerType
+	SDRC_EMissionDifficulty difficulty;		//Difficulty for specific mission
+	int xp;									//Experience given	
+	
+	//------------------------------------------------------------------------------------------------
+	void SDRC_MissionConfigGeneral()
+	{
+		SetDefaults();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void SetDefaults(int subIdx_ = -1, string comment_ = SDRC_DEFAULT, 
+					 vector pos_ = "0 0 0", float size_ = 5,
+					 array<EMapDescriptorType> locationTypes_ = null,
+					 string posName_ = SDRC_DEFAULT, string title_ = SDRC_DEFAULT, string info_ = SDRC_DEFAULT, 
+					 SDRC_EMissionWinCondition winCondition_ = SDRC_EMissionWinCondition.DEFAULT, 
+					 string winMessage_ = SDRC_DEFAULT, string loseMessage_ = SDRC_DEFAULT, 
+					 string faction_ = SDRC_DEFAULT, 
+					 string markerType_ = SDRC_DEFAULT, int markerIcon_ = -1, 
+					 SDRC_EMissionDifficulty difficulty_ = SDRC_EMissionDifficulty.NORMAL, 
+					 int xp_ = 0)
+	{
+		array<vector> pos_array = {pos_, "0 0 0"};
+		Set(subIdx_, comment_, pos_array, size_, locationTypes_, posName_, title_, info_, winCondition_, winMessage_, loseMessage_, faction_, markerType_, markerIcon_, difficulty_, xp_);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void Set(int subIdx_, string comment_, array<vector> pos_, float size_, array<EMapDescriptorType> locationTypes_, string posName_, string title_, string info_, SDRC_EMissionWinCondition winCondition_, string winMessage_, string loseMessage_, string faction_, string markerType_, int markerIcon_, SDRC_EMissionDifficulty difficulty_, int xp_)
+	{
+		subIdx = subIdx_;
+		comment = comment_;
+		pos = pos_;
+		size = size_;
+		if (locationTypes_)
+		{
+			locationTypes = locationTypes_;
+		}
+		posName = posName_;
+		title = title_;
+		info = info_;
+		winCondition = winCondition_;
+		winMessage = winMessage_;
+		loseMessage = loseMessage_;
+		faction = faction_;
+		markerType = markerType_;
+		markerIcon = markerIcon_;
+		difficulty = difficulty_;		
+		xp = xp_;
+	}	
+}
