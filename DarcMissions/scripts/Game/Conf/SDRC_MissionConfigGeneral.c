04350747f671f4f9e045a0f4764d9b0225d32658
mokdevel <mokdevel@hotmail.com>
2026-01-17 21:07:07 +0200

    WIP Json changes

--- DarcMissions/scripts/Game/Conf/SDRC_MissionConfigGeneral.c
+++ DarcMissions/scripts/Game/Conf/SDRC_MissionConfigGeneral.c
@@ -16,16 +16,28 @@ class SDRC_MissionConfigGeneral : Managed
 	SDRC_EMissionWinCondition winCondition;	//Mission win condidition
 	string winMessage;						//Message to show when mission is completed
 	string loseMessage;						//Message to show when mission fails
+#ifdef NEW_VERSION_WIP
+	ref array<string> faction = {};			//Faction for the mission. Setting as empty, works as the default to select from the enemyFactions
+#else
 	string faction;							//Faction for the mission. Setting as empty, works as the default to select from the enemyFactions
+#endif	
 	string markerType;						//Marker type for the mission
 	int markerIcon;							//Marker ID within markerType
+#ifdef NEW_VERSION_WIP
+	ref array<SDRC_EDifficulty> difficulty = {}; //Difficulty options for specific mission
+#else	
 	SDRC_EDifficulty difficulty;			//Difficulty for specific mission
+#endif	
 	int xp;									//Experience given	
+#ifdef NEW_VERSION_WIP
+	ref array<string> mods = {};					//List of mods needed for this mission
+#endif	
 	
 	//------------------------------------------------------------------------------------------------
 	void SDRC_MissionConfigGeneral()
 	{
 		SetDefaults();
+		SetDefaults2({});
 	}
 	
 	//------------------------------------------------------------------------------------------------
@@ -35,15 +47,23 @@ class SDRC_MissionConfigGeneral : Managed
 					 string posName_ = SDRC_DEFAULT, string title_ = SDRC_DEFAULT, string info_ = SDRC_DEFAULT, 
 					 SDRC_EMissionWinCondition winCondition_ = SDRC_EMissionWinCondition.DEFAULT, 
 					 string winMessage_ = SDRC_DEFAULT, string loseMessage_ = SDRC_DEFAULT, 
-					 string faction_ = SDRC_DEFAULT, 
+					 string faction_ = "", 
 					 string markerType_ = SDRC_DEFAULT, int markerIcon_ = -1, 
 					 SDRC_EDifficulty difficulty_ = SDRC_EDifficulty.RANDOM, 
-					 int xp_ = 0)
+					 int xp_ = 0 
+					 )
 	{
 		array<vector> pos_array = {pos_, "0 0 0"};
 		Set(subIdx_, comment_, pos_array, size_, locationTypes_, posName_, title_, info_, winCondition_, winMessage_, loseMessage_, faction_, markerType_, markerIcon_, difficulty_, xp_);
 	}
-	
+
+	void SetDefaults2(array<string> mods_)
+	{
+#ifdef NEW_VERSION_WIP
+		Set2(mods_);
+#endif	
+	}
+		
 	//------------------------------------------------------------------------------------------------
 	void Set(int subIdx_, string comment_, array<vector> pos_, float size_, array<EMapDescriptorType> locationTypes_, string posName_, string title_, string info_, SDRC_EMissionWinCondition winCondition_, string winMessage_, string loseMessage_, string faction_, string markerType_, int markerIcon_, SDRC_EDifficulty difficulty_, int xp_)
 	{
@@ -61,10 +81,33 @@ class SDRC_MissionConfigGeneral : Managed
 		winCondition = winCondition_;
 		winMessage = winMessage_;
 		loseMessage = loseMessage_;
+#ifdef NEW_VERSION_WIP
+		if (faction_ != "")			//TBD: The parameter will be an array so this needs to fixed to handle that
+		{
+			faction.Clear();		
+			faction.Insert(faction_);
+		}
+#else
 		faction = faction_;
+#endif
 		markerType = markerType_;
 		markerIcon = markerIcon_;
-		difficulty = difficulty_;		
+#ifdef NEW_VERSION_WIP		
+		if (difficulty_ != SDRC_EDifficulty.RANDOM)		//TBD: The parameter will be an array so this needs to fixed to handle that
+		{
+			difficulty.Clear();
+		}
+		difficulty.Insert(difficulty_);
+#else		
+		difficulty = difficulty_;
+#endif		
 		xp = xp_;
+	}
+	
+	void Set2(array<string> mods_)
+	{
+#ifdef NEW_VERSION_WIP
+		mods = mods_;
+#endif	
 	}	
 }
