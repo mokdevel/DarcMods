4e0ebabf455cb33ed20bfa253a7acccae19aeee2
mokdevel <mokdevel@hotmail.com>
2026-03-18 20:32:03 +0200

    DarcChopper items are under Systems

--- DarcChopper/scripts/Game/Components/SDRC_DarcChopperEditableRequestComp.c
+++ DarcChopper/scripts/Game/Components/SDRC_DarcChopperEditableRequestComp.c
@@ -7,7 +7,7 @@ Component to modify mission related information via GM. Used with GM placable ch
 NOTE: This file is currently not used for anything.
 */
 
-
+/*
 //------------------------------------------------------------------------------------------------
 [ComponentEditorProps(category: "GameScripted/Editor (Editables)", description: "", icon: "WBData/ComponentEditorProps/componentEditor.png")]
 class SDRC_DarcChopperEditableRequestCompClass : SCR_EditableSystemComponentClass { }
@@ -17,4 +17,8 @@ SDRC_DarcChopperEditableRequestCompClass g_DarcChopperEditableRequestClass;
 //! @ingroup Editable_Entities
 class SDRC_DarcChopperEditableRequestComp : SCR_EditableSystemComponent
 {
+	//NOTE: Params are set in SDRC_EditorAttributeBaseValues in file SDRC_BaseValueListEditorAttribute.c
+	[Attribute("-1", desc: "Just a number")]	
+	protected int m_iNumber;
 }
+*/
--- DarcChopper/scripts/Game/Editor/Enums/SDRC_EEditableEntityLabelDarcChopper.c
+++ DarcChopper/scripts/Game/Editor/Enums/SDRC_EEditableEntityLabelDarcChopper.c
@@ -1,4 +1,4 @@
 modded enum EEditableEntityLabel
 {
-	ENTITYTYPE_DARCCHOPPER,	// = 48302,		//TBD: Just a random number here. Don't really like guessing one.
+	ENTITYTYPE_DARCCHOPPER	// = 48302,		//TBD: Just a random number here. Don't really like guessing one.
 };
